11bef3d8066af2b73f29745af32fd537a9186c8f
Prashant Solanki <solankisrp2@gmail.com>
2022-12-11 16:20:45 +0000

    Fir lint issues

--- versions.tf
+++ versions.tf
@@ -4,5 +4,13 @@ terraform {
       source  = "Telmate/proxmox"
       version = "2.9.10"
     }
+    null = {
+      source = "hashicorp/null"
+      version = "3.2.1"
+    }
+    local = {
+      source = "hashicorp/local"
+      version = "2.2.3"
+    }
   }
 }
